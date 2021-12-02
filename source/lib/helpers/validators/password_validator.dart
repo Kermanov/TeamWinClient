42050fa828f06cdde1ba111763c4245bfde77478
Maksym Dovhan <kerman901@gmail.com>
2021-12-02 11:59:24 +0200

    Update name and password validation

--- source/lib/helpers/validators/password_validator.dart
+++ source/lib/helpers/validators/password_validator.dart
@@ -6,13 +6,10 @@ class Password extends FormzInput<String, PasswordValidationError> {
   const Password.pure() : super.pure('');
   const Password.dirty([String value = '']) : super.dirty(value);
 
-  static final _passwordRegExp =
-  RegExp(r'^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$');
+  static final _regExp = RegExp(r'^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$');
 
   @override
   PasswordValidationError validator(String value) {
-    return _passwordRegExp.hasMatch(value)
-        ? null
-        : PasswordValidationError.invalid;
+    return _regExp.hasMatch(value) ? null : PasswordValidationError.invalid;
   }
 }
